f1fdf5019511aeeae4c847ad9e328d4d4e65ca72
Denis Shefer <i-48220_den@mail.ru>
2022-01-18 22:59:53 +0300

    Reformat ACMP 0001

--- src/main/java/com/github/denmazila/java_learning/acmp/task0001/Main.java
+++ src/main/java/com/github/denmazila/java_learning/acmp/task0001/Main.java
@@ -1,16 +1,16 @@
 package com.github.denmazila.java_learning.acmp.task0001;
 
-import java.io.PrintWriter;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-       PrintWriter out = new PrintWriter(System.out);
+        PrintWriter out = new PrintWriter(System.out);
 
         int a = in.nextInt();
         int b = in.nextInt();
-        int c=a+b;
+        int c = a + b;
         out.print(c);
         out.flush();
     }
