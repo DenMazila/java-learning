ca4be03b8fe132aac4a8c37618c905831b82ce16
Denis Shefer <i-48220_den@mail.ru>
2022-01-20 19:54:43 +0300

    ACMP 0003

---
+++ src/main/java/com/github/denmazila/java_learning/acmp/task0003/Main.java
@@ -0,0 +1,25 @@
+package com.github.denmazila.java_learning.acmp.task0003;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        int n = in.nextInt();
+        int a = n / 10; // выделяем значение без последней цифры
+        if (a == 0) {
+            String c = "25";
+            out.print(c);
+        } else {
+            int b = a * (a + 1);
+            String c = b + "25";
+            out.print(c);
+        }
+
+        // out.print(c);
+        out.flush();
+    }
+}
