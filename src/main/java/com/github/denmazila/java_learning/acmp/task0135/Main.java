72b6bbec038ec6c9dbdc968528fb27b9f7588510
Denis Shefer <i-48220_den@mail.ru>
2022-03-27 22:05:49 +0300

    Временный пакет поддельное решение 135 задачи.

---
+++ src/main/java/com/github/denmazila/java_learning/acmp/task0135/Main.java
@@ -0,0 +1,17 @@
+package com.github.denmazila.java_learning.acmp.task0135;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = a + b;
+        out.print(c);
+        out.flush();
+    }
+}
