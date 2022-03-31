a1da2c27ff94d78dc1aba97ad393643b1916781a
Denis Shefer <i-48220_den@mail.ru>
2022-03-31 20:15:56 +0300

    acmp0013 привёл фаил в соответствии с правилами acmp

--- src/main/java/com/github/denmazila/java_learning/acmp/task0013/Main.java
+++ src/main/java/com/github/denmazila/java_learning/acmp/task0013/Main.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.print("Введите четырёхзначное натуральное число которое нужно отгадать: ");
+        //System.out.print("Введите четырёхзначное натуральное число которое нужно отгадать: ");
         int hiddenNumber = in.nextInt();
-        System.out.print("Введите четырёхзначное натуральное число предложенное отгадывающим: ");
+        //System.out.print("Введите четырёхзначное натуральное число предложенное отгадывающим: ");
         int suggestedNumber = in.nextInt();
         int[] hidden = Main.integerToArrayConversionHiddenNumber(hiddenNumber);
         int[] suggested = integerToArrayConversionSuggestedNumber(suggestedNumber);
-        System.out.println("Число быков:" + numberOfExactMatches(hidden, suggested) + " Число коров:" + numberOfIndirectMatches(hidden, suggested));
+        System.out.println(numberOfExactMatches(hidden, suggested) + " " + numberOfIndirectMatches(hidden, suggested));
     }
 
     public static int[] integerToArrayConversionHiddenNumber(int hiddenNumber) {
