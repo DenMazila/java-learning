4726676b1ea778ba6b6823ad85c36757477065e4
Denis Shefer <i-48220_den@mail.ru>
2022-04-28 23:16:26 +0300

    Добывил main, переименовал в scenario1.

--- src/main/java/com/github/denmazila/java_learning/electronic_school_diary/Main.java
+++ src/main/java/com/github/denmazila/java_learning/electronic_school_diary/Main.java
@@ -1,7 +1,7 @@
 package com.github.denmazila.java_learning.electronic_school_diary;
 
 public class  Main {
-    public static void main(String[] args) {
+    public static void scenario1 (String[] args) {
         System.out.println("Выберите класс:");
         String classSelected = "3Б";
         System.out.println("Выберите ученика:");
@@ -17,31 +17,6 @@ public class  Main {
         // Mark mark = new Mark(valueSelected, comment)
         // lesson.addMark(mark);
 
-
-
-
-
-
-        Account student1 = new Account("356", "3Б", "Шефер Полина Денисовна");
-        ElSchoolDiary maths = new Maths (4, "Домашняя работа");
-        ElSchoolDiary maths2 = new Maths (5, "Контрольная");
-        ElSchoolDiary maths3 = new Maths (3, "Работа на уроке");
-        ElSchoolDiary maths4 = new Maths();
-        ElSchoolDiary russianLanguage1 = new RussianLanguage (5, "диктант");
-        ElSchoolDiary russianLanguage2 = new RussianLanguage (4, "Работа на уроке");
-        ElSchoolDiary russianLanguage3 = new RussianLanguage (5, "диктант");
-        ElSchoolDiary russianLanguage4 = new RussianLanguage (2, "домашняя работа");
-        ElSchoolDiary russianLanguage5 = new RussianLanguage (5, 4, "сочинение");
-        ElSchoolDiary physicalCulture1 = new PhysicalCulture (5, "работа на уроке");
-        ElSchoolDiary physicalCulture2 = new PhysicalCulture (5, "пресс");
-        ElSchoolDiary physicalCulture3 = new PhysicalCulture (4, "Работа на уроке");
-        ElSchoolDiary physicalCulture4 = new PhysicalCulture (5, "бег на дистанцию 30м.");
-
-
-        //maths.addMark()
-        System.out.println("Средняя оценка по математике:" + maths3.calculateAverage());
-        System.out.println("Средняя оценка по русскому языку:" + russianLanguage5.calculateAverage());
-        System.out.println("Средняя оценка по русскому физической культуре:" + physicalCulture4.calculateAverage());
     }
 
 }
